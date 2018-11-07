8a320333180635e6f8254b1935169b26a1be9b06
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2018-11-07 16:43:09 -0500

    v before default values and fs

--- liri.js
+++ liri.js
@@ -3,50 +3,37 @@ var Request = require("request");
 var Spotify = require("node-spotify-api");
 var Bandsintown = require('bandsintown')('codingbootcamp');
 var keys = require("./keys.js");
+var moment=require("moment")
+var fs = require("fs");
+
 var spotify = new Spotify(keys.spotify);
 
+//User input
 var inputString = process.argv;
 var command = inputString[2];
 var userChoice =inputString.slice(3).join(" ");
 console.log(userChoice);
-//var info = process.argv[3];
 
-if(command === "concert-this"){
-//node liri.js concert-this <artist/band name here>
-bandInfo()
-// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
-}
-  else if(command === "spotify-this-song"){
-//node liri.js spotify-this-song '<song name here>'
-  songInfo();
-//else genre === undefined default info about "The Sign" by Ace of Base
-  }
-  else if(command === "movie-this"){
-    movieInfo();
-//else default:
-//If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-//It's on Netflix!
-  }
-  else if(command === "do-what-it-says"){
-    //doIt();
-        //fs package
-        //LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands
-        //output:
-        //spotify-this-song for "I Want it That Way," as follows the text in random.txt
-        //test  movie-this 
-      }
+//input user's commands,it switches to the related one 
+if(command === "concert-this"){bandInfo()}
+else if(command === "spotify-this-song"){songInfo()}
+else if(command === "movie-this"){movieInfo();}
+else if(command === "do-what-it-says"){doIt();}
+
+
 function bandInfo(){
   console.log("results for a song: "+userChoice) 
   Request.get(`http://rest.bandsintown.com/artists/${userChoice}/events?app_id=codingbootcamp`,
     function(err, res, body){
-    console.log(JSON.parse(body));
+     if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    console.log("Venue name: " + JSON.parse(body)[0].venue.name);
+    console.log("Venue location: " + JSON.parse(body)[0].venue.country +" , " + JSON.parse(body)[0].venue.city);
+    var date=JSON.parse(body)[0].datetime;
+    console.log("Date time: " + moment(date).format('MM DD YYYY'));
 
   });
-  
-
 };
 
 
@@ -54,21 +41,25 @@ function songInfo(){
   console.log("results for a song: "+userChoice)  
  
    spotify.search({ type: 'track', query: userChoice }, function(err, data) {
+
     if (err) {
       return console.log('Error occurred: ' + err);
     }
+    console.log(query);
     var result = data.tracks.items[0];
 
     console.log("///////////////////////////////////");
     console.log('Artists: ' + result.album.artists[0].name)
     console.log('Song name: ' + result.name)
     console.log('Preview: ' + result.album.href)
-    console.log('Album: ' + result.album.name)      
+    console.log('Album: ' + result.album.name) 
+  
   })
+
 };
 
 function movieInfo (){
-  console.log("results for a song: "+userChoice)  
+  console.log("results for a movie: "+userChoice)  
 
   Request( "http://www.omdbapi.com/?t="+userChoice+"&y=&plot=short&apikey=trilogy",
   function(error, response, body){
@@ -76,7 +67,7 @@ function movieInfo (){
       var result=JSON.parse(body);
 
       console.log("///////////////////////////////////");
-      console.log("Title: " + result.Title);//parse Json string and returns a JavaScript object
+      console.log("Title: " + result.Title);
       console.log("Release Year: " + result.Year);
       console.log("IMDB Rating: " + result.imdbRating);
       console.log("Rotten Tomatoes: " + result.Ratings[1].Value);
@@ -85,9 +76,24 @@ function movieInfo (){
       console.log("Plot: "+ result.Plot);
       console.log("Actors: "+ result.Actors);
     }
+    else{
+      console.log("If you haven't watched "+"Mr. Nobody,"+ "then you should: http://www.imdb.com/title/tt0485947/")
+      console.log("It's on Netflix!");
+    }
   })
 };
 
+function doIt(){
+  fs.readFile("random.txt", "utf8", function(error, data){
+    if (error) {
+      return console.log(error)
+    }
+      var dataArr = data.split(",");
+      console.log(dataArr);
+      var command=dataArr[0];
+      var userChoice=dataArr[1];
+  });
+}
 
 
 
